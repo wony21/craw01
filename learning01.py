d4e0e19805af95e25736bc0c263e2718ddc39e0d
최재원 <njaewon@nate.com>
2020-07-29 16:20:49 +0900

    조선일보 크롤링 DB 수집

--- learning01.py
+++ learning01.py
@@ -4,6 +4,8 @@ import requests
 import datetime
 import pymongo
 
+
+print('Program start')
 source = requests.get('https://www.chosun.com/').content
 soup = BeautifulSoup(source, 'html.parser', from_encoding='utf-8')
 node1 = soup.find_all('dl', {'class':'news_item'})
@@ -25,11 +27,16 @@ for node in node1:
             }
             news_list.append(item)
 
+for news in news_list:
+    print(news)
+
 host = 'localhost'
 port = 27017
 
+print('DB insert...')
 connection = pymongo.MongoClient(host, port)
 database = connection.get_database('moya')
 collection = database.get_collection('news')
 collection.insert_many(news_list)
 connection.close()
+print('Program completed.')
