65f02ce6701003ab1f03802fff4cef826a01ce10
최재원 <njaewon@nate.com>
2020-07-30 16:54:05 +0900

    Href 링크 추가

--- learning01.py
+++ learning01.py
@@ -9,6 +9,15 @@ print('Program start')
 source = requests.get('https://www.chosun.com/').content
 soup = BeautifulSoup(source, 'html.parser', from_encoding='utf-8')
 node1 = soup.find_all('dl', {'class':'news_item'})
+
+# source = requests.get('https://joongang.joins.com/').content
+# soup = BeautifulSoup(source, 'html.parser', from_encoding='utf-8')
+# node1 = soup.find_all('ul', {'class':'list_vertical'})
+
+# source = requests.get('https://www.donga.com/').content
+# soup = BeautifulSoup(source, 'html.parser', from_encoding='utf-8')
+# node1 = soup.find_all('span', {'class':'txt'})
+
 kkma = Kkma()
 now = datetime.datetime.now()
 today = now.strftime("%Y%m%d%H%M%S")
@@ -18,14 +27,25 @@ for node in node1:
     dt_rows = node.find_all('dt')
     for dt in dt_rows:
         news = dt.text.replace('\n', '')
+        a = dt.find('a')
         if len(news) > 0:
             words = kkma.nouns(news)
             item = {
                 "news_date" : today,
                 "news_text" : news,
-                "words" : words
+                "words" : words,
+                'href' : a['href']
             }
             news_list.append(item)
+    # news = node.text.replace('\n', '')
+    # if len(news) > 0:
+    #     words = kkma.nouns(news)
+    #     item = {
+    #         "news_date" : today,
+    #         "news_text" : news,
+    #         "words" : words
+    #     }
+    #     news_list.append(item)
 
 for news in news_list:
     print(news)
